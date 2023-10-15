261a4cfacb47dbe965039c8ccb59bc2b6ccae839
obold <p240188@gmail.com>
2023-10-16 02:24:42 +0500

    teach

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const someText = 'some text'
-console.log(typeof someText)
-
-const someNumber = 5
-console.log(typeof someNumber)
+// const age = prompt('Напишите ваш вес(кг): ');
+// let height = prompt('Введите ваш рост(м): ');
+const age=15;
+const isAdult = age>= 18;5
+if (isAdult) {tvvv,g3
+5
+  console.log('You can watch this video');
+} else {
+  console.log(`You can't watch this video, you can wait ${18-age} years`);
+}1
+111
